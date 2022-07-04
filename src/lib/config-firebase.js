bf7124192eef62d8f52edf70b6da736a9ab8617f
Noribel <rivero.noribel@gmail.com>
2022-07-04 12:22:49 -0400

    conf-firebase

---
+++ src/lib/config-firebase.js
@@ -0,0 +1,31 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+
+  debugger
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCDpuP5qukXQmDeBaajneDqsNKeExXm4kw",
+    authDomain: "social-network-66b05.firebaseapp.com",
+    projectId: "social-network-66b05",
+    storageBucket: "social-network-66b05.appspot.com",
+    messagingSenderId: "789128338041",
+    appId: "1:789128338041:web:13997401099ed6de6d52a8",
+    measurementId: "G-PWCQHWPKNT"
+  };
+
+  // Initialize Firebase
+  export function configInitial() { 
+  const app = initializeApp(firebaseConfig);
+  return { app };
+}
+  //const auth = getAuth(app);
+  //const provider = GoogleAuthProvider(app);
+
+  
+  //export default app
