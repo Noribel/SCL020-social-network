8141faa338a8590a27d1577814028eecf07ed4b2
Noribel <rivero.noribel@gmail.com>
2022-07-04 12:10:17 -0400

    ruteado

---
+++ src/lib/router.js
@@ -0,0 +1,24 @@
+import { login } from '../componentes/login.js';
+import { register } from '../componentes/register.js';
+import { wall } from '../componentes/wall.js';
+
+
+export const router = (hash) => {
+    
+    const rootBox = document.getElementById('root');
+    rootBox.innerHTML = ''; 
+
+
+    if (hash === '#/' || hash === '/' || hash === '#' || hash === '') {
+      rootBox.appendChild(login());
+      //login(); 
+    } else if (hash === '#/login') {
+      rootBox.appendChild(login());
+      //login();
+    } else if (hash === '#/register') {
+      rootBox.appendChild(register());
+      //eventsRegister(); 
+    } else if (hash === '#/wall') {
+      rootBox.appendChild(wall());
+    }
+  };
