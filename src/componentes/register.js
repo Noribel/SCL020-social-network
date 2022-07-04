b2fb85844bce7511a47decc1fed5eddc37842bf0
Noribel <rivero.noribel@gmail.com>
2022-07-04 12:14:35 -0400

    creando página de registro

---
+++ src/componentes/register.js
@@ -0,0 +1,37 @@
+export const register = () => {
+
+    const pageRegister =
+   
+    `
+   <div class="register">
+      <header></header>
+       <div class="logo">
+    <h1>MUJERES CONTRA VIENTO Y MAREA</h1>
+        <img class="imgLogo" src="imagenes/logo.webp">
+    </div>
+       <form name="registerForm" type="submit" class="formRegister">
+       <input class="name" type="name" placeholder="Nombre y Apellido">
+       <input class="email" type="name" placeholder="Correo electrónico válido">
+       <input class="contraseña" type="name" placeholder="Contraseña">
+       <input class="contraseña" type="name" placeholder="Repetir contraseña">
+       <p>Al registrarte, aceptas términos y condiciones de uso.</p>
+       <button class="iniciarSesion">Registrar</button>
+       </form>
+       <div class="back">
+       <p href="#/login">¿Tines cuenta? Inicia Sesión</p>
+       <button class="iniciarSesion">Iniciar Sesión</button> 
+       </div>
+  
+   <footer>Realizado por Noribel Rivero. Derechos Reservados</footer>
+   
+   
+   </div>
+       `
+       ;
+   
+       const containerRegister = document.createElement('div');
+       containerRegister.classList.add('containerRegister');
+       containerRegister.innerHTML = pageRegister;
+       return containerRegister;
+   
+   };
